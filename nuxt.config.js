9ef492aabacdea469f0a6c0354f12765980f6c4b
Bondok6 <kirolousbondok6@gmail.com>
2022-04-17 05:36:02 +0200

    Install gh-pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,4 +37,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  target: "static",
+  router: {
+    base: "/nuxt-todo/",
+  },
 };
--- pages/home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h3>fasdf</h3>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
